61ac334a76a484f23a7bc70476556289ff05f5f5
Luiz Vasconcelos <luizvasconceloss@yahoo.com.br>
2018-10-02 10:37:06 +0200

    testing

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,6 @@
 class profile::base {
 
   #the base profile should include component modules that will be on all nodes
-
+$hiera_testing = lookup('key1')
+notify { "hiera key1 value is: ${hiera_testing}": }
 }
